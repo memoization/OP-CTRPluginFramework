7e5d098fa5d50d32ab36a23a83a68e09bd557ba6
Nanquitas <nath.doidi@gmail.com>
2020-07-27 19:32:52 +0000

    Time: Move GetCurrent to public function

--- Library/include/CTRPluginFramework/System/Time.hpp
+++ Library/include/CTRPluginFramework/System/Time.hpp
@@ -16,14 +16,14 @@ namespace CTRPluginFramework
 
         s64     AsMicroseconds(void) const;
 
-
-        static const Time Zero; ///< Predefined "zero" time value
+        static Time         GetCurrent(void);
+        static const Time   Zero; ///< Predefined "zero" time value
 
     private :
 
         friend      Time Seconds(float amount);
         friend      Time Milliseconds(int amount);
-        friend      Time Microseconds(s64 amount);   
+        friend      Time Microseconds(s64 amount);
 
         explicit    Time(s64 microseconds);
 
@@ -33,24 +33,24 @@ namespace CTRPluginFramework
         s64     _microseconds;
     };
 
-  
+
     Time Seconds(float amount);
 
- 
+
     Time Milliseconds(int amount);
 
- 
+
     Time Microseconds(s64 amount);
- 
+
     bool operator ==(Time left, Time right);
 
-  
+
     bool operator !=(Time left, Time right);
 
- 
+
     bool operator <(Time left, Time right);
 
- 
+
     bool operator >(Time left, Time right);
 
 
--- Library/source/CTRPluginFramework/System/Time.cpp
+++ Library/source/CTRPluginFramework/System/Time.cpp
@@ -1,6 +1,15 @@
 #include "types.h"
-
+#include <3ds.h>
 #include "CTRPluginFramework/System/Time.hpp"
+
+#define TICKS_PER_SEC       268123480
+#define SYSCLOCK_SOC       (16756991)
+#define SYSCLOCK_ARM9      (SYSCLOCK_SOC * 8)
+#define SYSCLOCK_ARM11     (SYSCLOCK_ARM9 * 2)
+#define SYSCLOCK_ARM11_NEW (SYSCLOCK_ARM11 * 3)
+
+#define CPU_TICKS_PER_MSEC (SYSCLOCK_ARM11 / 1000.0)
+#define CPU_TICKS_PER_USEC (SYSCLOCK_ARM11 / 1000000.0)
 
 namespace CTRPluginFramework
 {
@@ -16,65 +25,59 @@ namespace CTRPluginFramework
         return (_microseconds / 1000000.f);
     }
 
-
     int     Time::AsMilliseconds(void) const
     {
         return static_cast<int>(_microseconds / 1000);
     }
-
 
     s64     Time::AsMicroseconds(void) const
     {
         return (_microseconds);
     }
 
-
     Time::Time(s64 microseconds) : _microseconds(microseconds)
     {
     }
 
+    Time    Time::GetCurrent(void)
+    {
+        return Microseconds(svcGetSystemTick() / CPU_TICKS_PER_USEC);
+    }
 
     Time    Seconds(float amount)
     {
         return (Time(static_cast<s64>(amount * 1000000)));
     }
 
-
     Time    Milliseconds(int amount)
     {
         return (Time(static_cast<s64>(amount) * 1000));
     }
-
 
     Time    Microseconds(s64 amount)
     {
         return (Time(amount));
     }
 
-
     bool operator ==(Time left, Time right)
     {
         return (left.AsMicroseconds() == right.AsMicroseconds());
     }
-
 
     bool operator !=(Time left, Time right)
     {
         return (left.AsMicroseconds() != right.AsMicroseconds());
     }
 
-
     bool operator <(Time left, Time right)
     {
         return (left.AsMicroseconds() < right.AsMicroseconds());
     }
 
-
     bool operator >(Time left, Time right)
     {
         return (left.AsMicroseconds() > right.AsMicroseconds());
     }
-
 
     bool operator <=(Time left, Time right)
     {
@@ -91,106 +94,88 @@ namespace CTRPluginFramework
         return (Microseconds(-right.AsMicroseconds()));
     }
 
-
     Time operator +(Time left, Time right)
     {
         return (Microseconds(left.AsMicroseconds() + right.AsMicroseconds()));
     }
-
 
     Time& operator +=(Time& left, Time right)
     {
         return (left = left + right);
     }
 
-
     Time operator -(Time left, Time right)
     {
         return (Microseconds(left.AsMicroseconds() - right.AsMicroseconds()));
     }
-
 
     Time& operator -=(Time& left, Time right)
     {
         return left = left - right;
     }
 
-
     Time operator *(Time left, float right)
     {
         return (Seconds(left.AsSeconds() * right));
     }
-
 
     Time operator *(Time left, s64 right)
     {
         return (Microseconds(left.AsMicroseconds() * right));
     }
 
-
     Time operator *(float left, Time right)
     {
         return (right * left);
     }
-
 
     Time operator *(s64 left, Time right)
     {
         return (right * left);
     }
 
-
     Time& operator *=(Time& left, float right)
     {
         return (left = left * right);
     }
-
 
     Time& operator *=(Time& left, s64 right)
     {
         return (left = left * right);
     }
 
-
     Time operator /(Time left, float right)
     {
         return Seconds(left.AsSeconds() / right);
     }
-
 
     Time operator /(Time left, s64 right)
     {
         return (Microseconds(left.AsMicroseconds() / right));
     }
 
-
     Time& operator /=(Time& left, float right)
     {
         return (left = left / right);
     }
-
 
     Time& operator /=(Time& left, s64 right)
     {
         return (left = left / right);
     }
 
-
     float operator /(Time left, Time right)
     {
         return (left.AsSeconds() / right.AsSeconds());
     }
-
 
     Time operator %(Time left, Time right)
     {
         return (Microseconds(left.AsMicroseconds() % right.AsMicroseconds()));
     }
 
-
     Time& operator %=(Time& left, Time right)
     {
         return (left = left % right);
     }
-
 }
